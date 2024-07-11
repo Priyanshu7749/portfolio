13bd0bfc8b8b5a010329b5266b9cad88c325151b
priyanshu jethanandani <100209324+Priyanshu7749@users.noreply.github.com>
2024-07-11 14:57:36 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Approach from "@/components/Approach";
+import Client from "@/components/Client";
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
+import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <main className="relative flex justify-center bg-black-100 items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+      <div className=" max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
+        <Hero/>
+        <Grid/>
+        <RecentProjects />
+        <Approach/>
+        <Footer/>
+      </div>
+    </main>
+  );
+}
